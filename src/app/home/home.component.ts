fafb3257e4a392105177a0626ae66af34230f2d3
YunusEmreUslu <usluyunusemre00@gmail.com>
2024-07-26 16:43:51 +0300

    connected application with the node.js server and successfully received product information in console

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Products } from '../../types';
+import { ProductsService } from './../services/products.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor(
+    private ProductsService: ProductsService
+  ) {}
+
+  ngOnInit() {
+    this.ProductsService
+      .getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5})
+      .subscribe((products: Products) => {
+        console.log(products.items);
+    });
+  }
+}
